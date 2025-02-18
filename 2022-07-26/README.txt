4bc216855e743327eaef5ce381ccfa03d6bc3f49
Romeo Rizzi <romeo.rizzi@univr.it>
2025-02-18 11:29:50 +0100

    riallineate le pubblicazioni di appelli precedenti

---
+++ 2022-07-26/README.txt
@@ -0,0 +1,2 @@
+Questo appello, come altri appelli precedenti di cui non presenzia nemmeno il folder, era stato condotto su calcolatore.
+Per un periodo avevamo adottato una modalità diversa, che potesse anche essere da telematico, per via del CoVid.
---
+++ update_archive.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+for d in ../temi/20* ; do
+    folder=$(basename $d)
+    if [ ! -d "$folder" ]; then
+        mkdir $folder
+        ln -t $folder $d/*.pdf ;
+    fi    
+done;
+for f in 20*/*tema.pdf ; do
+    ln -s -r -t temi_all $f ;
+done;
+for f in 20*/*corr.pdf ; do
+    ln -s -r -t corr_all/$f ;
+done;
+git add .
+msg="automatic commit from script update_archive.sh $(date +%Y-%m-%d:%Hh:%Mm:%Ss)"
+git commit -m $msg
+git push
